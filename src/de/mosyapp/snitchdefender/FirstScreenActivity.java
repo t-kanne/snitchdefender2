ef3ea3853aa4fd5a18421f41019fb452e69554cd
prgrichi <richard.holzner@gmail.com>
2014-06-16 01:17:18 +0200

    kleine Änderungen

--- src/de/mosyapp/snitchdefender/FirstScreenActivity.java
+++ src/de/mosyapp/snitchdefender/FirstScreenActivity.java
@@ -2,15 +2,16 @@ package de.mosyapp.snitchdefender;
 
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
 @SuppressLint("NewApi")
-public class FirstScreenActivity extends Activity {
+public class FirstScreenActivity extends ActionBarActivity {
 	
 	Button nextButton;
 
@@ -19,6 +20,9 @@ public class FirstScreenActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.fragment_first_screen);
+		
+		ActionBar ab = getSupportActionBar();
+		ab.hide();
 		
 		nextButton = (Button) findViewById(R.id.nextbutton);
 		buttonListener();
