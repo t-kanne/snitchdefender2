49c2344490eead0278834720ea90fd93dc7759c9
Thomas <thomas.kanne-schludde@haw-hamburg.de>
2014-06-16 01:52:52 +0200

    Fullscreen schwarze Activity

--- src/de/mosyapp/snitchdefender/DimmActivity.java
+++ src/de/mosyapp/snitchdefender/DimmActivity.java
@@ -3,8 +3,11 @@ package de.mosyapp.snitchdefender;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -13,11 +16,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 import android.os.Build;
 import android.preference.PreferenceManager;
 
-public class DimmActivity extends ActionBarActivity {
+public class DimmActivity extends Activity {
 	private boolean isLockScreenDisabled;
+	private boolean doubleBackToExitPressedOnce;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +41,11 @@ public class DimmActivity extends ActionBarActivity {
 			
 			winParams.screenBrightness = WindowManager.LayoutParams.BRIGHTNESS_OVERRIDE_OFF;
 			win.setAttributes(winParams);
-			 
+			
+	        requestWindowFeature(Window.FEATURE_NO_TITLE);
+	        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+	            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+	        
 			setContentView(R.layout.activity_dimm);
 		}
 		else {
@@ -44,5 +53,27 @@ public class DimmActivity extends ActionBarActivity {
 		}
 
 	}
+	
+	@Override
+	public void onBackPressed() {	
+		stopService(new Intent(this, ActivateCountDownTimer.class));
+		
+		if (doubleBackToExitPressedOnce) {
+	        super.onBackPressed();
+	        return;
+	    }
+
+	    this.doubleBackToExitPressedOnce = true;
+	    Toast.makeText(this, "Nochmal drücken, um zurück zur App zu gelangen", Toast.LENGTH_SHORT).show();
+
+	    new Handler().postDelayed(new Runnable() {
+
+	        @Override
+	        public void run() {
+	            doubleBackToExitPressedOnce=false;                       
+	        }
+	    }, 2000);
+	}
+	
 
 }
