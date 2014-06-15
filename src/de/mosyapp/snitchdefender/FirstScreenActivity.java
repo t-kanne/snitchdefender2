96964c964f11083766e295008a4636c1071438cc
prgrichi <richard.holzner@gmail.com>
2014-06-16 01:03:27 +0200

    FirstScreenActivity verändert

--- src/de/mosyapp/snitchdefender/FirstScreenActivity.java
+++ src/de/mosyapp/snitchdefender/FirstScreenActivity.java
@@ -3,34 +3,11 @@ package de.mosyapp.snitchdefender;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.ServiceConnection;
-import android.content.SharedPreferences;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.IBinder;
-import android.preference.PreferenceManager;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
-import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 @SuppressLint("NewApi")
 public class FirstScreenActivity extends Activity {
@@ -49,7 +26,6 @@ public class FirstScreenActivity extends Activity {
 	
 	
 	public void buttonListener() { 
-		nextButton = (Button) findViewById(R.id.nextbutton);
 		nextButton.setOnClickListener(new OnClickListener() {
  
 			public void onClick(View v) {					
