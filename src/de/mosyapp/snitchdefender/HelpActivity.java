b240a5075f647a7321ae108c17bd86efc58336f2
prgrichi <richard.holzner@gmail.com>
2014-06-21 15:59:19 +0200

    Anpassungen in help, und credits

--- src/de/mosyapp/snitchdefender/HelpActivity.java
+++ src/de/mosyapp/snitchdefender/HelpActivity.java
@@ -1,9 +1,5 @@
 package de.mosyapp.snitchdefender;
 
-
-
-
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
@@ -15,8 +11,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
-  
 
 public class HelpActivity extends ActionBarActivity {
 
